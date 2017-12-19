f88aee72e83909bcf1c2066a2d3503eb5eaf545b
Pavle Gartner <pavlinio2a@gmail.com>
2017-12-19 14:50:14 +0100

    initial commit

---
+++ DeepSkyDad.AstroMonitor/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Owin.Hosting;
+using Nexmo.Api;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScreenshotApi
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Web server
+            string baseAddress = "http://*:7777/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine("Web api running on http://*:7777//api/screenshot/take. Press any key to stop AstroMonitor");
+
+                if (ConfigurationManager.AppSettings["PHD2.Monitor"] == "1")
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        Console.WriteLine("Starting monitoring...");
+                        try
+                        {
+                            int? _starLostCount = null;
+                            int _starLostCountCurr;
+                            FileInfo _file = null;
+                            FileInfo _fileLatest;
+                            while (true)
+                            {
+                                var directory = new DirectoryInfo(ConfigurationManager.AppSettings["PHD2.Monitor.LogFolder"]);
+                                _fileLatest = directory.GetFiles()
+                                    .Where(f => Regex.IsMatch(f.Name, @"PHD2_DebugLog_(\d{4})-(\d{2})-(\d{2})_\d{6}\.txt"))
+                                    .OrderByDescending(f => f.LastWriteTime)
+                                    .First();
+
+                                Console.Write($"{DateTime.Now.ToString("MM.dd.yyyy HH:mm")} Scanning file {_fileLatest.FullName}...");
+
+                                using (var fs = new FileStream(_fileLatest.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                                using (var sr = new StreamReader(fs, Encoding.Default))
+                                {
+                                    _starLostCountCurr = Regex.Matches(sr.ReadToEnd(), "Star lost").Count;
+                                }
+
+                                if (_file == null || _file.FullName != _fileLatest.FullName)
+                                {
+                                    //first time
+                                    _starLostCount = _starLostCountCurr;
+                                    _file = _fileLatest;
+                                    Console.WriteLine($" initial scan finished at {DateTime.Now.ToString("MM.dd.yyyy HH:mm")}, initial star lost count: {_starLostCount} ... sleep 30s");
+                                    Thread.Sleep(30000);
+                                }
+                                else
+                                {
+                                    if (_starLostCountCurr> (_starLostCount + 10))
+                                    {
+                                        
+                                        Console.WriteLine($"... scan finished at {DateTime.Now.ToString("MM.dd.yyyy HH: mm")} -  !!!!! STAR LOST !!!!! ({_starLostCountCurr} > {_starLostCount})");
+                                        var results = SMS.Send(new SMS.SMSRequest
+                                        {
+                                            from = "Telescope",
+                                            to = ConfigurationManager.AppSettings["Nexmo.recipient"],
+                                            text = "PHD Alert: star lost"
+                                        });
+
+                                        Console.WriteLine($"SMS sent to {ConfigurationManager.AppSettings["Nexmo.recipient"]} ... sleep 10min");
+                                        Thread.Sleep(1000 * 60 * 10);
+                                        
+                                        //reset scan
+                                        _file = null;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"... scan finished at {DateTime.Now.ToString("MM.dd.yyyy HH:mm")} - OK ... sleep 30s");
+                                        Thread.Sleep(30000);
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Monitoring failed: {e.Message} Trace: {e.StackTrace}");
+                        }
+                    });
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DeepSkyDad.AstroMonitor/ScreenshotController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows.Forms;
+
+namespace ScreenshotApi
+{
+    public class ScreenshotController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Take()
+        {
+            //Will contain screenshot
+            Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics screenshotGraphics = Graphics.FromImage(screenshot);
+            //Make the screenshot
+            screenshotGraphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            MemoryStream ms = new MemoryStream();
+            screenshot.Save(ms, ImageFormat.Png);
+
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content = new ByteArrayContent(ms.ToArray());
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return result;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Ping()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "OK");
+        }
+    }
+}
